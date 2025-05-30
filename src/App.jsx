c0384db1c2502c337a14fee7909adc9874c9cdb1
airme1 <emmanuelochoja@yahoo.com>
2025-05-30 19:23:36 +0100

    carousel and room components

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,18 @@
-import EmblaCarousel from "./components/Carousel";
 import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
+import "./components/embla.css";
+// import room1Img from "./assets/room1.jpg";
+// import room2Img from "./assets/room2.jpg";
+// import room3Img from "./assets/room3.jpg";
+// import room4Img from "./assets/room4.jpg";
+import Spaces from "./components/Spaces";
 
 function App() {
   return (
     <>
       <Navbar />
       <Hero />
-      <EmblaCarousel />
+      <Spaces space="Our Room"/>
     </>
   );
 }
--- src/components/Spaces.jsx
+++ src/components/Spaces.jsx
@@ -1,10 +1,19 @@
-import React from 'react';
+import room1Img from "../assets/room1.jpg";
+import room2Img from "../assets/room2.jpg";
+import room3Img from "../assets/room3.jpg";
+import room4Img from "../assets/room4.jpg";
+import EmblaCarousel from './EmblaCarousel';
+
+
+const OPTIONS = { loop: true };
+const SLIDE_COUNT = 4;
+const SLIDES = [room1Img, room2Img, room3Img, room4Img];
 
 export default function Spaces({space}){
     return (
-        <section className="spaces">
-            <h2>{space}</h2>
-            
+        <section className="spaces my-8">
+            <h2 className='text-4xl font-bold'>{space}</h2>
+            <EmblaCarousel slides={SLIDES}/>
         </section>
     )
 }
