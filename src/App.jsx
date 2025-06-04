85e81d1658e0a952f1b44f047d99f2585f8c4383
airme1 <emmanuelochoja@yahoo.com>
2025-06-04 10:56:11 +0100

    Facilities, testimonials, utilities and gallery components

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,38 @@
 import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
 import "./components/embla.css";
-// import room1Img from "./assets/room1.jpg";
-// import room2Img from "./assets/room2.jpg";
-// import room3Img from "./assets/room3.jpg";
-// import room4Img from "./assets/room4.jpg";
 import Spaces from "./components/Spaces";
+import room1Img from "./assets/room1.jpg";
+import room2Img from "./assets/room2.jpg";
+import room3Img from "./assets/room3.jpg";
+import room4Img from "./assets/room4.jpg";
+import hall1Img from "./assets/hall1.jpg";
+import hall2Img from "./assets/hall2.jpg";
+import hall3Img from "./assets/hall3.jpg";
+import hall4Img from "./assets/hall4.jpg";
+import Review from "./Review";
+import Utilities from "./Utilities";
+import ImageGallery from "./ImageGallery";
+
+const ROOMSLIDES = [room1Img, room2Img, room3Img, room4Img];
+const HALLSLIDES = [hall1Img, hall2Img, hall3Img, hall4Img];
 
 function App() {
   return (
     <>
       <Navbar />
       <Hero />
-      <Spaces space="Our Room"/>
+      <Spaces space="Our Rooms" slideImgs={ROOMSLIDES} />
+      <Spaces space="Our Facilities" slideImgs={HALLSLIDES} />
+      <Review
+        title="Proven Track of Satisfied Clients"
+        subtext="See what our clients are saying"
+      />
+      <Utilities
+        title="Designed For Your Comfort"
+        subtext="Every detail is thoughtfully arranged to make your stay smooth, secure, and stress-free"
+      />
+      <ImageGallery />
     </>
   );
 }
--- src/components/Spaces.jsx
+++ src/components/Spaces.jsx
@@ -1,19 +1,14 @@
-import room1Img from "../assets/room1.jpg";
-import room2Img from "../assets/room2.jpg";
-import room3Img from "../assets/room3.jpg";
-import room4Img from "../assets/room4.jpg";
 import EmblaCarousel from './EmblaCarousel';
 
 
 const OPTIONS = { loop: true };
 const SLIDE_COUNT = 4;
-const SLIDES = [room1Img, room2Img, room3Img, room4Img];
 
-export default function Spaces({space}){
+export default function Spaces({space, slideImgs}){
     return (
         <section className="spaces my-8">
             <h2 className='text-4xl font-bold'>{space}</h2>
-            <EmblaCarousel slides={SLIDES}/>
+            <EmblaCarousel slides={slideImgs}/>
         </section>
     )
 }
