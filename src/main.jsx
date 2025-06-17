dc8404c0eb13450b3857025ec71e82c61b2b5aa4
airme1 <emmanuelochoja@yahoo.com>
2025-06-17 19:29:57 +0100

    updated main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,25 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import Home from "./pages/Home.jsx";
+import { BrowserRouter, Route, Routes } from "react-router";
+import About from "./pages/About.jsx";
+import Service from "./pages/Service.jsx";
+import Book from "./pages/Book.jsx";
+import Navbar from "./components/Navbar.jsx";
+import NotFound from "./pages/NotFound.jsx";
+import Room from "./pages/rooms/Room.jsx";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+createRoot(document.getElementById("root")).render(
+  <BrowserRouter>
+    <Navbar />
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="about" element={<About />} />
+      <Route path="service" element={<Service />} />
+      <Route path="book" element={<Book />} />
+      <Route path="room/:id" element={<Room />} />
+      <Route path="*" element={<NotFound />} />
+    </Routes>
+  </BrowserRouter>
+);
