256acf156009f9c2da941f632b2c3429612922a4
Ochoja <danielochoja@yahoo.com>
2025-07-02 15:16:37 +0100

    updated Home page

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,14 +1,22 @@
+import { Icon } from '@iconify/react';
+import { useState } from 'react';
+
 export default function Navbar() {
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
     <nav className='layout-pad py-4 flex justify-between items-center border-b-primary/20 border-1'>
       <h1 className='text-2xl font-bold'>Bakhita Pastoral Center</h1>
-      <div className='flex gap-6 items-center'>
+      <div className='lg:flex gap-6 items-center hidden'>
         <div>Home</div>
         <div>About</div>
         <div>Our Services</div>
         <button className='bg-[#0e0e0e] text-white px-4 py-2 rounded-full'>
           Book Now
         </button>
+      </div>
+      <div className='lg:hidden' onClick={() => setIsOpen(!isOpen)}>
+        <Icon icon='mingcute:menu-line' className='text-3xl cursor-pointer' />
       </div>
     </nav>
   );
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,16 +1,16 @@
-import React, { useRef, useEffect } from "react";
-import HeroImage from "../assets/topViewHotel.png";
-import contactImg from "../assets/contactImg.png";
-import { Icon } from "@iconify/react";
-import rooms from "../assets/data/rooms.json";
-import emailIcon from "../assets/icons/email.svg";
-import phoneIcon from "../assets/icons/phone.svg";
-import addressIcon from "../assets/icons/address.svg";
-import facilities from "../assets/data/facilities.json";
-import { Swiper, SwiperSlide } from "swiper/react";
-import { Navigation } from "swiper/modules";
-import "swiper/css";
-import "swiper/css/navigation";
+import React, { useRef, useEffect } from 'react';
+import HeroImage from '../assets/topViewHotel.png';
+import contactImg from '../assets/contactImg.png';
+import { Icon } from '@iconify/react';
+import rooms from '../assets/data/rooms.json';
+import emailIcon from '../assets/icons/email.svg';
+import phoneIcon from '../assets/icons/phone.svg';
+import addressIcon from '../assets/icons/address.svg';
+import facilities from '../assets/data/facilities.json';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Navigation } from 'swiper/modules';
+import 'swiper/css';
+import 'swiper/css/navigation';
 
 export default function Home() {
   // Separate refs for Rooms
@@ -69,8 +69,8 @@ export default function Home() {
     <main>
       {/* Hero Section */}
       <section>
-        <div className="layout-pad mt-[5%] mb-16 text-center lg:max-w-[70%] xl:max-w-[65%] mx-auto">
-          <h1 className="font-bold text-4xl md:text-6xl mb-4">
+        <div className='layout-pad mt-[5%] mb-16 text-center lg:max-w-[70%] xl:max-w-[65%] mx-auto'>
+          <h1 className='font-bold text-4xl md:text-6xl mb-4'>
             Welcome to Bakhita Pastoral Center
           </h1>
           <p>
@@ -78,59 +78,51 @@ export default function Home() {
             our calling. Nestled in the heart of Sokoto, our serene and secure
             environment offers more than just accommodation.
           </p>
-          <button className="bg-[#0e0e0e] text-white px-8 py-2 rounded-full mt-4">
+          <button className='bg-[#0e0e0e] text-white px-8 py-2 rounded-full mt-4'>
             Book Now
           </button>
         </div>
 
-        <div className="w-full">
-          <img src={HeroImage} alt="Hero Image" className="w-full h-auto" />
+        <div className='w-full'>
+          <img src={HeroImage} alt='Hero Image' className='w-full h-auto' />
         </div>
       </section>
 
       {/* Rooms Section */}
-      <section className="mt-16">
-        <div className="layout-pad flex justify-between items-center">
-          <h2 className="font-bold text-4xl">Our Rooms</h2>
-          <div className="flex gap-4 items-center text-primary text-4xl">
+      <section className='mt-16'>
+        <div className='layout-pad flex justify-between items-center'>
+          <h2 className='font-bold text-4xl'>Our Rooms</h2>
+          <div className='flex gap-4 items-center text-primary text-4xl'>
             <Icon
-              icon="solar:round-arrow-left-line-duotone"
-              className="cursor-pointer"
+              icon='solar:round-arrow-left-line-duotone'
+              className='cursor-pointer'
               ref={roomPrevRef}
             />
             <Icon
-              icon="solar:round-arrow-right-line-duotone"
-              className="cursor-pointer"
+              icon='solar:round-arrow-right-line-duotone'
+              className='cursor-pointer'
               ref={roomNextRef}
             />
           </div>
         </div>
 
-        <div className="mt-6 pl-[5%] xl:pl-[8%]">
+        <div className='mt-6 pl-[5%] xl:pl-[8%] flex'>
           <Swiper
             modules={[Navigation]}
             spaceBetween={30}
-            slidesPerView={3}
+            slidesPerView={2.5}
             loop={false}
-            onSwiper={(swiper) => (roomSwiperRef.current = swiper)}
-            breakpoints={{
-              0: { slidesPerView: 1 },
-              640: { slidesPerView: 2 },
-              1024: { slidesPerView: 3 },
-            }}
-          >
+            onSwiper={(swiper) => (roomSwiperRef.current = swiper)}>
             {rooms.map((room, index) => (
               <SwiperSlide key={index}>
                 <div>
-                  <div className="w-full h-48 md:h-56 rounded-xl overflow-hidden mb-3 bg-gray-100 flex items-center justify-center">
-                    <img
-                      src={room.images[0]}
-                      alt=""
-                      className="w-full h-full object-cover"
-                    />
-                  </div>
-                  <h3 className="text-xl font-semibold mb-2">{room.name}</h3>
-                  <p className="text-sm text-gray-600">{room.price}</p>
+                  <img
+                    src={room.images[0]}
+                    alt=''
+                    className='w-full h-96 rounded-xl mb-3 object-cover'
+                  />
+                  <div className='text-lg font-semibold mb-1'>{room.name}</div>
+                  <p className='text-sm text-gray-600'>{room.price}</p>
                 </div>
               </SwiperSlide>
             ))}
@@ -139,44 +131,42 @@ export default function Home() {
       </section>
 
       {/* Facilities Section */}
-      <section className="mt-32">
-        <div className="layout-pad flex justify-between items-center">
-          <h2 className="font-bold text-4xl">Our Facilities</h2>
-          <div className="flex gap-4 items-center text-primary text-4xl">
+      <section className='mt-32'>
+        <div className='layout-pad flex justify-between items-center'>
+          <h2 className='font-bold text-4xl'>Our Facilities</h2>
+          <div className='flex gap-4 items-center text-primary text-4xl'>
             <Icon
-              icon="solar:round-arrow-left-line-duotone"
-              className="cursor-pointer"
+              icon='solar:round-arrow-left-line-duotone'
+              className='cursor-pointer'
               ref={facilityPrevRef}
             />
             <Icon
-              icon="solar:round-arrow-right-line-duotone"
-              className="cursor-pointer"
+              icon='solar:round-arrow-right-line-duotone'
+              className='cursor-pointer'
               ref={facilityNextRef}
             />
           </div>
         </div>
 
-        <div className="mt-6 pl-[5%] xl:pl-[8%]">
+        <div className='mt-6 pl-[5%] xl:pl-[8%]'>
           <Swiper
             modules={[Navigation]}
             spaceBetween={30}
-            slidesPerView={3}
+            slidesPerView={2.5}
             loop={false}
-            onSwiper={(swiper) => (facilitySwiperRef.current = swiper)}
-            breakpoints={{
-              0: { slidesPerView: 1 },
-              640: { slidesPerView: 2 },
-              1024: { slidesPerView: 3 },
-            }}
-          >
+            onSwiper={(swiper) => (facilitySwiperRef.current = swiper)}>
             {facilities.map((facility, index) => (
               <SwiperSlide key={index}>
-                <div className="p-4 bg-white shadow-md rounded-xl">
-                  <img src={facility.images[0]} alt={facility.name} />
-                  <h3 className="text-xl font-semibold mb-2">
+                <div>
+                  <img
+                    src={facility.images[0]}
+                    alt={facility.name}
+                    className='w-full h-96 rounded-xl mb-3 object-cover'
+                  />
+                  <div className='text-lg font-semibold mb-1'>
                     {facility.name}
-                  </h3>
-                  <p className="text-sm text-gray-600">{facility.headline}</p>
+                  </div>
+                  <p className='text-sm text-gray-600'>{facility.headline}</p>
                 </div>
               </SwiperSlide>
             ))}
@@ -185,56 +175,56 @@ export default function Home() {
       </section>
 
       {/* Testimonials Section */}
-      <section className="layout-pad mt-32">
-        <h2 className="font-bold text-4xl text-center">
+      <section className='layout-pad mt-32'>
+        <h2 className='font-bold text-4xl text-center'>
           Proven Track of Satisfied Clients
         </h2>
-        <p className="text-center mt-2">See what our clients are saying</p>
+        <p className='text-center mt-2'>See what our clients are saying</p>
 
-        <div className="flex flex-wrap gap-8 items-stretch mt-8">
-          <div className="bg-white border-primary/30 rounded-2xl border flex flex-col justify-between py-6 px-4 flex-1 md:min-w-[320px] gap-4">
-            <Icon icon="el:quote-alt" className="text-accent text-3xl" />
+        <div className='flex flex-wrap gap-8 items-stretch mt-8'>
+          <div className='bg-white border-primary/30 rounded-2xl border flex flex-col justify-between py-6 px-4 flex-1 md:min-w-[320px] gap-4'>
+            <Icon icon='el:quote-alt' className='text-accent text-3xl' />
             <div>
-              <div className="font-semibold text-lg">
+              <div className='font-semibold text-lg'>
                 Perfect Spot for Events and Stay
               </div>
-              <div className="font-light">
+              <div className='font-light'>
                 Our wedding reception was held at Bakhita and everything was
                 perfect. Thank you for making our day magical!
               </div>
             </div>
 
             <div>
-              <div className="font-medium">- Grace and Musa</div>
-              <div className="font-light text-sm">Couple</div>
+              <div className='font-medium'>- Grace and Musa</div>
+              <div className='font-light text-sm'>Couple</div>
             </div>
           </div>
 
-          <div className="bg-white border-primary/30 rounded-2xl border flex flex-col justify-between py-6 px-4 flex-1 md:min-w-[320px] gap-4">
-            <Icon icon="el:quote-alt" className="text-accent text-3xl" />
+          <div className='bg-white border-primary/30 rounded-2xl border flex flex-col justify-between py-6 px-4 flex-1 md:min-w-[320px] gap-4'>
+            <Icon icon='el:quote-alt' className='text-accent text-3xl' />
             <div>
-              <div className="font-semibold text-lg">
+              <div className='font-semibold text-lg'>
                 Felt Safe and Comfortable
               </div>
-              <div className="font-light">
+              <div className='font-light'>
                 The peace I felt staying here was unmatched. From the
                 cleanliness to the friendly staff — I’ll definitely return.
               </div>
             </div>
 
             <div>
-              <div className="font-medium">- Dr Sani M</div>
-              <div className="font-light text-sm">Business Man</div>
+              <div className='font-medium'>- Dr Sani M</div>
+              <div className='font-light text-sm'>Business Man</div>
             </div>
           </div>
 
-          <div className="bg-white border-primary/30 rounded-2xl border flex flex-col justify-between py-6 px-4 flex-1 md:min-w-[320px] gap-4">
-            <Icon icon="el:quote-alt" className="text-accent text-3xl" />
+          <div className='bg-white border-primary/30 rounded-2xl border flex flex-col justify-between py-6 px-4 flex-1 md:min-w-[320px] gap-4'>
+            <Icon icon='el:quote-alt' className='text-accent text-3xl' />
             <div>
-              <div className="font-semibold text-lg">
+              <div className='font-semibold text-lg'>
                 Perfect Spot for Events and Stay
               </div>
-              <div className="font-light">
+              <div className='font-light'>
                 We hosted a birthday party at the event hall and booked a few
                 rooms for our guests — everyone had a great time. Reliable
                 amenities and responsive staff made a big difference. It’s rare
@@ -243,70 +233,70 @@ export default function Home() {
             </div>
 
             <div>
-              <div className="font-medium">- Paulina</div>
-              <div className="font-light text-sm">Event Planner</div>
+              <div className='font-medium'>- Paulina</div>
+              <div className='font-light text-sm'>Event Planner</div>
             </div>
           </div>
         </div>
       </section>
 
-      <section className="layout-pad mt-32">
-        <h2 className="font-bold text-4xl text-center">
+      <section className='layout-pad mt-32'>
+        <h2 className='font-bold text-4xl text-center'>
           Designed For Your Comfort
         </h2>
-        <p className="text-center mt-2">
+        <p className='text-center mt-2'>
           Every detail is thoughtfully arranged to make your stay smooth,
           secure, and stress-free.
         </p>
 
-        <div className="mt-16 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-8 lg:max-w-[90%] mx-auto">
+        <div className='mt-16 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-8 lg:max-w-[90%] mx-auto'>
           {/* Electricity */}
-          <div className="flex flex-col items-center text-center gap-3">
+          <div className='flex flex-col items-center text-center gap-3'>
             <Icon
-              icon="healthicons:electricity-outline"
-              className="text-primary text-5xl"
+              icon='healthicons:electricity-outline'
+              className='text-primary text-5xl'
             />
-            <h3 className="text-xl md:text-2xl font-bold">
+            <h3 className='text-xl md:text-2xl font-bold'>
               24 Hour Electricity
             </h3>
-            <p className="text-sm font-light">
+            <p className='text-sm font-light'>
               Reliable power supply around the clock for your comfort.
             </p>
           </div>
 
           {/* Parking */}
-          <div className="flex flex-col items-center text-center gap-3">
+          <div className='flex flex-col items-center text-center gap-3'>
             <Icon
-              icon="hugeicons:car-parking-01"
-              className="text-primary text-5xl"
+              icon='hugeicons:car-parking-01'
+              className='text-primary text-5xl'
             />
-            <h3 className="text-xl md:text-2xl font-bold">Private Parking</h3>
-            <p className="text-sm font-light">
+            <h3 className='text-xl md:text-2xl font-bold'>Private Parking</h3>
+            <p className='text-sm font-light'>
               Enjoy hassle-free parking at no extra cost.
             </p>
           </div>
 
           {/* Internet */}
-          <div className="flex flex-col items-center text-center gap-3">
-            <Icon icon="uiw:wifi" className="text-primary text-5xl" />
-            <h3 className="text-xl md:text-2xl font-bold">
+          <div className='flex flex-col items-center text-center gap-3'>
+            <Icon icon='uiw:wifi' className='text-primary text-5xl' />
+            <h3 className='text-xl md:text-2xl font-bold'>
               High Speed Internet
             </h3>
-            <p className="text-sm font-light">
+            <p className='text-sm font-light'>
               Stay connected seamlessly across our spaces.
             </p>
           </div>
 
           {/* Security */}
-          <div className="flex flex-col items-center text-center gap-3">
+          <div className='flex flex-col items-center text-center gap-3'>
             <Icon
-              icon="hugeicons:security-lock"
-              className="text-primary text-5xl"
+              icon='hugeicons:security-lock'
+              className='text-primary text-5xl'
             />
-            <h3 className="text-xl md:text-2xl font-bold">
+            <h3 className='text-xl md:text-2xl font-bold'>
               Top-notch Security
             </h3>
-            <p className="text-sm font-light">
+            <p className='text-sm font-light'>
               Your safety is guaranteed with our 24/7 security systems.
             </p>
           </div>
@@ -314,10 +304,10 @@ export default function Home() {
       </section>
 
       {/* Contact Section */}
-      <section className="layout-pad mt-32 mb-20">
-        <div className="text-center mb-8 lg:text-left lg:max-w-[55%]">
-          <h2 className="font-bold text-4xl">We're here to help</h2>
-          <p className="text-sm mt-4">
+      <section className='layout-pad mt-32 mb-20'>
+        <div className='text-center mb-8 lg:text-left lg:max-w-[55%]'>
+          <h2 className='font-bold text-4xl'>We're here to help</h2>
+          <p className='text-sm mt-4'>
             Ready to book? Whether you're planning an event, retreat, or
             extended stay, we're here to help. Call or WhatsApp us today to
             reserve your space—rooms, halls, and services are available, and
@@ -325,36 +315,36 @@ export default function Home() {
           </p>
         </div>
 
-        <div className="lg:flex lg:justify-between items-center lg:gap-16 xl:gap-28">
-          <div className="md:w-[60%] mx-auto lg:w-full xl:max-w-[40%]">
-            <div className="bg-primary/15 flex items-center gap-6 py-3 px-6 rounded-full mb-4">
-              <img src={emailIcon} alt="Email Icon" />
+        <div className='lg:flex lg:justify-between items-center lg:gap-16 xl:gap-28'>
+          <div className='md:w-[60%] mx-auto lg:w-full xl:max-w-[40%]'>
+            <div className='bg-primary/15 flex items-center gap-6 py-3 px-6 rounded-full mb-4'>
+              <img src={emailIcon} alt='Email Icon' />
               <div>
-                <div className="font-medium mb-2">Email</div>
-                <div className="text-sm">info@bakhitapastoralcentre.com</div>
+                <div className='font-medium mb-2'>Email</div>
+                <div className='text-sm'>info@bakhitapastoralcentre.com</div>
               </div>
             </div>
-            <div className="bg-primary/15 flex items-center gap-6 py-3 px-6 rounded-full mb-4">
-              <img src={phoneIcon} alt="Phone Icon" />
+            <div className='bg-primary/15 flex items-center gap-6 py-3 px-6 rounded-full mb-4'>
+              <img src={phoneIcon} alt='Phone Icon' />
               <div>
-                <div className="font-medium mb-2">Phone</div>
-                <div className="text-sm">+234 817 006 1600</div>
+                <div className='font-medium mb-2'>Phone</div>
+                <div className='text-sm'>+234 817 006 1600</div>
               </div>
             </div>
-            <div className="bg-primary/15 flex items-center gap-6 py-3 px-6 rounded-full mb-2">
-              <img src={addressIcon} alt="Address Icon" />
+            <div className='bg-primary/15 flex items-center gap-6 py-3 px-6 rounded-full mb-2'>
+              <img src={addressIcon} alt='Address Icon' />
               <div>
-                <div className="font-medium mb-2">Address</div>
-                <div className="text-sm">456 Bakhita way, Sokoto, Nigeria</div>
+                <div className='font-medium mb-2'>Address</div>
+                <div className='text-sm'>456 Bakhita way, Sokoto, Nigeria</div>
               </div>
             </div>
           </div>
 
-          <div className="hidden lg:block">
+          <div className='hidden lg:block'>
             <img
               src={contactImg}
-              alt="Location"
-              className="h-full w-auto rounded-2xl"
+              alt='Location'
+              className='h-full w-auto rounded-2xl'
             />
           </div>
         </div>
