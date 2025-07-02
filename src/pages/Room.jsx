f03451402f7d098dfaf2626eeffe652f3ea0ef86
Ochoja <danielochoja@yahoo.com>
2025-07-02 22:53:03 +0100

    updated rooms page

--- src/pages/Room.jsx
+++ src/pages/Room.jsx
@@ -46,18 +46,45 @@ export default function Room() {
         </div>
 
         <div className='relative z-10 text-center py-[30vh]'>
-          <h2 className='text-5xl font-bold mb-2'>{room.name}</h2>
+          <h1 className='text-5xl font-bold mb-2'>{room.name}</h1>
           <p>{room.price}</p>
         </div>
       </header>
 
-      <section className='mt-16'>
-        <div className='layout-pad flex justify-between items-center'>
-          <h2 className='font-bold text-4xl'>Our Rooms</h2>
-          <div className='flex gap-4 items-center text-primary text-4xl'></div>
+      <section className='layout-pad mt-8'>
+        <h2 className='font-semibold text-2xl mb-2'>{room.headline}</h2>
+        <p>{room.details}</p>
+
+        <div className='mt-4 flex flex-wrap gap-6'>
+          {room.amenities.map((item, index) => {
+            const [label, icon] = Object.entries(item)[0];
+            return (
+              <div
+                key={index}
+                className='flex gap-4 items-center border-primary/40 border rounded-xl px-4 py-3'>
+                <Icon icon={icon} className='text-3xl text-primary' />
+                <span>{label}</span>
+              </div>
+            );
+          })}
         </div>
       </section>
-      <p className='text-md text-gray-600'>Book your stay with us today!</p>
+
+      <section>
+        <div className='layout-pad flex justify-between items-center mt-20'>
+          <h2 className='text-4xl font-bold'>Gallery</h2>
+          <div className='flex gap-4 items-center text-primary text-4xl'>
+            <Icon
+              icon='solar:round-arrow-left-line-duotone'
+              className='cursor-pointer'
+            />
+            <Icon
+              icon='solar:round-arrow-right-line-duotone'
+              className='cursor-pointer'
+            />
+          </div>
+        </div>
+      </section>
     </main>
   );
 }
